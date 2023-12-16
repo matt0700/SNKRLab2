52dade39944da073be3d314654b7c9a3c11039f3
matt0700 <89130475+matt0700@users.noreply.github.com>
2023-12-17 00:45:44 +0800

    Proceed Button and Second Empty Activity

--- app/src/main/java/com/example/snkrlab2/MainActivity.java
+++ app/src/main/java/com/example/snkrlab2/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.snkrlab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button Prcd_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+ 
+        Prcd_button = findViewById(R.id.Proceed_Button);
+        Prcd_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
