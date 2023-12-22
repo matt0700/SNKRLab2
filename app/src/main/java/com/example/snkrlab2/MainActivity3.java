daae3180935516136c4ba014fff1e0e0c33a7a0f
Patrick Sevilla <102337806+KamikazeGoBoom@users.noreply.github.com>
2023-12-22 23:18:16 +0800

    Blue Mic,Hide, and Setting Button

--- app/src/main/java/com/example/snkrlab2/MainActivity3.java
+++ app/src/main/java/com/example/snkrlab2/MainActivity3.java
@@ -1,14 +1,60 @@
 package com.example.snkrlab2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity3 extends AppCompatActivity {
+
+    private boolean isMicClicked = false;
+    private boolean isHideClicked = false;
+    private boolean isSettingsClicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        final ImageButton micButton = findViewById(R.id.voiceButton);
+        final ImageButton hideButton = findViewById(R.id.hideUnhideButton);
+        final ImageButton settingsButton = findViewById(R.id.settingsButton);
+
+        micButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isMicClicked) {
+                    micButton.setImageResource(R.drawable.mic);
+                } else {
+                    micButton.setImageResource(R.drawable.blue_mic);
+                }
+                isMicClicked = !isMicClicked;
+            }
+        });
+
+        hideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isHideClicked) {
+                    hideButton.setImageResource(R.drawable.hide);
+                } else {
+                    hideButton.setImageResource(R.drawable.blue_hide);
+                }
+                isHideClicked = !isHideClicked;
+            }
+        });
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isSettingsClicked) {
+                    settingsButton.setImageResource(R.drawable.setting);
+                } else {
+                    settingsButton.setImageResource(R.drawable.blue_setting);
+                }
+                isSettingsClicked = !isSettingsClicked;
+            }
+        });
     }
 }
